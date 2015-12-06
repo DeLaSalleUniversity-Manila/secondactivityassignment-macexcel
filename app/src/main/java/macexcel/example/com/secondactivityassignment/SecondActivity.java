54ff3608fd706acb2992106d1d0debe3614b4204
macexcel <mexcelsf@yahoo.com>
2015-12-06 13:52:21 +0800

    commit

---
+++ app/src/main/java/macexcel/example/com/secondactivityassignment/SecondActivity.java
@@ -0,0 +1,29 @@
+package macexcel.example.com.secondactivityassignment;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by root on 9/29/15.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        handleIntent();
+    }
+
+    private void handleIntent(){
+        Intent i = getIntent();
+        String day_out = i.getStringExtra("message");
+        TextView tv_out = (TextView) findViewById(R.id.textViewOut);
+        tv_out.setText(day_out);
+    }
+
+}
